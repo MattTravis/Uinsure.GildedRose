9531f62c716cc6b0f70db47194fe721b6aa4115b
Matthew Travis <Matthew.Travis@brighthr.com>
2025-06-01 20:36:22 +0100

    Feat (QualityStrategy): Integrate the QualityStrategy in GildedRose.

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -1,4 +1,6 @@
-﻿namespace GildedRoseKata;
+﻿using GildedRoseKata.QualityStrategy;
+
+namespace GildedRoseKata;
 
 public class GildedRose
 {
@@ -10,112 +12,20 @@ public class GildedRose
 
     public void UpdateItems()
     {
-        UpdateQualityPreSellIn();
-        UpdateSellIn();
-        UpdateQualityPostSellIn();
-    }
-
-    private void UpdateQualityPreSellIn()
-    {
         foreach (var item in Items)
         {
-            if (item.Name == "Conjured Mana Cake")
-            {
-                if (item.Quality > 0)
-                {
-                    item.Quality -= 1;
-                }
-            }
+            var strategy = QualityStrategyFactory.Create(item.Name, item.SellIn);
+            strategy.Apply(item);
 
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality -= 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality += 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-                    }
-                }
-            }
+            UpdateSellIn(item);
         }
     }
 
-    private void UpdateSellIn()
+    private static void UpdateSellIn(Item item)
     {
-        foreach (var item in Items)
+        if (item.Name != "Sulfuras, Hand of Ragnaros")
         {
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn -= 1;
-            }
-        }
-    }
-
-    private void UpdateQualityPostSellIn()
-    {
-        foreach (var item in Items)
-        {
-            if (item.SellIn < 0)
-            {
-                if (item.Name == "Conjured Mana Cake")
-                {
-                    if (item.Quality > 0)
-                    {
-                        item.Quality -= 1;
-                    }
-                }
-
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality -= item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
-                }
-            }
+            item.SellIn -= 1;
         }
     }
 }
